2bef5b9861d39f76597c818e6ccb4dc5925934e0
unknown <tjtik83@gmail.com>
2021-03-26 08:31:12 +0700

    first commit

---
+++ src/components/TattooItem.js
@@ -0,0 +1,12 @@
+import './TattooItem.css'
+
+function TattooItem() {
+    return (
+      <div className="tattoo-item">
+        <img src="/images/download (2).png" />
+        <h4>ต่ายน้อย 1</h4>
+      </div>
+    );
+}
+
+export default TattooItem;
---
+++ src/components/TattooPost.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './TattooPost.css';
+
+function TattooPost() {
+  return (
+    <div className="tattoo-post">
+      <div className="tattoo-post-bg" />
+      <div className="tattoo-post-content">
+        <img src="/images/download (2).png" />
+        <h4>ต่ายน้อย 1</h4>
+      </div>
+    </div>
+  );
+}
+
+export default TattooPost;
